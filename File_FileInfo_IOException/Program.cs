5a9ac7f736c619f5c76d41abd4c65949e6d77868
Sergio Luis Coutinho Ferreira <sergiolc.ferreira@hotmail.com>
2020-07-21 13:05:11 -0300

    File,FileInfo e IOException

---
+++ File_FileInfo_IOException/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Runtime.ConstrainedExecution;
+using System;
+
+
+namespace File_FileInfo_IOException
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            string sourcePath = @"C:\TEMP\file1.txt";
+
+            string targetPath = @"C:\TEMP\file2.txt";
+
+            try {
+
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (var item in lines) {
+
+                    Console.WriteLine(item);
+
+                }
+
+            } catch (IOException e) {
+
+                Console.WriteLine("An error occured");
+
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
